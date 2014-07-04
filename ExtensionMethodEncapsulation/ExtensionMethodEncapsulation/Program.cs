ff84ea0fc4ffb3d16180fd6ee078a1b9c4fbcfcc
benjamin-wss <benjamin.wss@gmail.com>
2014-07-04 20:38:08 +0800

    Nuget packages setup and initial sample code is now in place.

--- ExtensionMethodEncapsulation/ExtensionMethodEncapsulation/Program.cs
+++ ExtensionMethodEncapsulation/ExtensionMethodEncapsulation/Program.cs
@@ -1,15 +1,36 @@
-﻿namespace ExtensionMethodEncapsulation
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+﻿using System;
+using System.Globalization;
 
+namespace ExtensionMethodEncapsulation
+{
     class Program
     {
+        public class SomeKindaPoco
+        {
+            public int Id { get; set; }
+            public string SomeKindaMessage { get; set; }
+        }
+
         static void Main(string[] args)
         {
+            var obj = new SomeKindaPoco()
+            {
+                Id = 1,
+                SomeKindaMessage = "Skullduggery"
+            };
+
+            var jsonString = obj.SerializeAsJsonString();
+
+            Console.WriteLine("Now we got the POCO into a JSON string in ONE LINE via our brand spankin new extension method. !! Whee !!!!");
+            Console.WriteLine(jsonString);
+
+            var strongTypedJsonObject = jsonString.DeserializeJsonString<SomeKindaPoco>();
+
+            Console.WriteLine("Now we got the JSON string back into a POCO via our extension method., WHEEEEE !!!!");
+            Console.WriteLine("ID Property : " + strongTypedJsonObject.Id.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine("SomeKindaMessageProperty : " + strongTypedJsonObject.SomeKindaMessage);
+
+            Console.ReadLine();
         }
     }
 }
